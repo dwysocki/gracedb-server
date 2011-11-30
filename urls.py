3cdfc71fe7c08c7ba42e288a33241af185bf0d3c
Brian Moe <brian.moe@ligo.org>
2011-11-30 16:21:26 -0600

    Django 1.1 to 1.3 mods

--- urls.py
+++ urls.py
@@ -37,7 +37,6 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    url(r'^admin/', include(admin.site.urls)),
 
 )
