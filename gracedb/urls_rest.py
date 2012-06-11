4c6e993c31847cbacbf889605aef1f9fb4dc5fc2
Brian Moe <brian.moe@ligo.org>
2012-06-11 15:15:51 -0500

    typo

--- gracedb/urls_rest.py
+++ gracedb/urls_rest.py
@@ -2,5 +2,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gracedb.api',
-    url (r'^event/(?P<graceid>[\w\d]+)/files/(?P<filename>.+)?$', 'download', name="download"),
+    url (r'^events/(?P<graceid>[\w\d]+)/files/(?P<filename>.+)?$', 'download', name="download"),
 )
