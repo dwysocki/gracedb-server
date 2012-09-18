30974fae07858be2fa6e0a6db61247ff63f2f020
Brian Moe <brian.moe@ligo.org>
2012-09-18 15:39:49 -0500

    Added settings for a dev site for Branson

---
+++ settings/branson.py
@@ -0,0 +1,54 @@
+
+CONFIG_NAME = "Branson"
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default' : {
+        'NAME'     : 'branson',
+        'ENGINE'   : 'django.db.backends.mysql',
+        'USER'     : 'gracedb',
+        'PASSWORD' : 'thinglet',
+    }
+}
+
+
+ALERT_EMAIL_FROM = "Dev Alert <root@moe.phys.uwm.edu>"
+ALERT_EMAIL_TO = [
+    "Branson Stephens <branson@gravity.phys.uwm.edu>",
+    ]
+ALERT_EMAIL_BCC = ["branson@gravity.phys.uwm.edu"]
+
+ALERT_TEST_EMAIL_FROM = "Dev Test Alert <root@moe.phys.uwm.edu>"
+ALERT_TEST_EMAIL_TO = [
+    "Branson Stephens <branson@gravity.phys.uwm.edu>",
+    ]
+
+# Don't sent out non-test XMPP alerts on dev box!
+XMPP_ALERT_CHANNELS = [
+                        'test_omega',
+                        'test_mbtaonline',
+                        'test_cwb',
+                        'test_lowmass',
+                      ]
+ 
+# SkyAlert
+SKYALERT_IVORN_PATTERN = "ivo://ligo.org/gracedb#%s-dev"
+
+# Latency histograms.  Where they go and max latency to bin.
+LATENCY_REPORT_DEST_DIR = "/home/branson/data/latency"
+LATENCY_REPORT_WEB_PAGE_FILE_PATH = LATENCY_REPORT_DEST_DIR + "/latency.inc"
+
+# Uptime reporting
+UPTIME_REPORT_DIR = "/homebransonbmoe/data/uptime"
+
+
+SITE_ID = 4
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    "/homebransonbmoe/gracedb/templates",
+)
