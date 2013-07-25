254dee04f8686128be7fe1202e37698496352442
Brian Moe <brian.moe@ligo.org>
2013-07-25 14:21:37 -0500

    Fix bug #980 extra_attributes missing in event lists

--- gracedb/api.py
+++ gracedb/api.py
@@ -422,7 +422,7 @@ class EventList(APIView):
                 d = {'error': 'Invalid query' }
                 return Response(d,status=status.HTTP_400_BAD_REQUEST)
 
-        events = events.order_by(sort)
+        events = events.order_by(sort).select_subclasses()
 
         start = int(start)
         count = int(count)
