8623161bbf4bed96089b18b9c9144ff5a748630c
Branson Stephens <stephenb@uwm.edu>
2014-02-14 17:13:14 -0600

    Added a log file and view for the ER5 performance metric. The PerformanceMiddleware response handler checks whether the user tried to create an event or annotation, and records the status code of the response. The view 'performance' displays stats for performance over the last three days. NB: This solution is fragile for several different reasons. But the most egregious is that permissions on the log file cause conflicts with django management commands. This will need to be addressed in the future.

---
+++ middleware/performance.py
@@ -0,0 +1,39 @@
+from django.core.urlresolvers import resolve
+import logging
+
+class PerformanceMiddleware:
+
+    #logging.basicConfig(filename="/home/branson/logs/performance.log",level=logging.INFO)
+
+    def process_response(self, request, response):
+        # Determine whether the user tried to create or replace an event.
+        logger = logging.getLogger(__name__)
+        url_name = resolve(request.path_info).url_name
+        create = False
+        if url_name=='create':
+            create = True
+        elif url_name=='event-list' and request.method=='POST':
+            create = True
+        elif url_name=='event-detail' and request.method=='PUT':
+            create = True
+
+        # Determine whether the user tried to annotate an event.
+        annotate = False
+        if url_name=='logentry':
+            annotate = True
+        elif url_name=='eventlog-list' and request.method=='POST':
+            annotate = True
+
+        # XXX If both are true, something is really wrong and there should
+        # be an error message.
+        if create and annotate:
+            return response
+
+        if create:
+            # Log the status.
+            #logging.info("%d" % response.status_code)
+            logger.info("create: %d: %s" % (response.status_code, request.user.username))
+        elif annotate:
+            logger.info("annotate: %d: %s" % (response.status_code, request.user.username))
+
+        return response
