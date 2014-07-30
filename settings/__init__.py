c328d957205316535f55d2e103a86491150a55af
Branson Stephens <branson.stephens@ligo.org>
2014-07-30 11:23:44 -0500

    Removed special update wsgi file and settings/__init__.py entry.

--- settings/__init__.py
+++ settings/__init__.py
@@ -29,9 +29,6 @@ configs = {
     '/home/branson/gracedbdev': 'branson',
     '/home/branson/gracedbdev/gracedb': 'branson',
 
-    '/home/branson/djangoupdate': 'update',
-    '/home/branson/djangoupdate/gracedb': 'update',
-
     "/home/gracedb/gracestage" : "stage",
 
 }
