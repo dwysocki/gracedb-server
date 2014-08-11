9c548f373c0c1068f39cdb03853d6a4ff5bb6d5f
Branson Stephens <branson.stephens@ligo.org>
2014-08-11 16:09:04 -0500

    Pointed guardian branch to db specifically for guardian.

--- settings/branson.py
+++ settings/branson.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default' : {
-        'NAME'     : 'gracedb',
+        'NAME'     : 'guardian',
         'ENGINE'   : 'django.db.backends.mysql',
         'USER'     : 'branson',
         'PASSWORD' : 'thinglet',
