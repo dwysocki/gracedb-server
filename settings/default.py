5c81cc85026845064796a20cf149d66ed7e82c39
Branson Stephens <branson.stephens@ligo.org>
2014-10-02 09:25:49 -0500

    Added init_commad option to branson.py and defaults.py

--- settings/default.py
+++ settings/default.py
@@ -59,6 +59,9 @@ DATABASES = {
         'ENGINE'   : 'django.db.backends.mysql',
         'USER'     : 'gracedb',
         'PASSWORD' : 'redrum4x',
+        'OPTIONS'  : {
+            'init_command' : 'SET storage_engine=MyISAM',
+        }
     }
 }
 
