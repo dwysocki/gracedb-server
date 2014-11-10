e07acdad153070d42b33377bd462da4fac03bd28
Branson Stephens <branson.stephens@ligo.org>
2014-11-10 11:07:11 -0600

    Added user_is_internal to template context, so as to avoid showing links in nav bar to pages which are inaccessible

--- gracedb/middleware/auth.py
+++ gracedb/middleware/auth.py
@@ -119,7 +119,18 @@
 #        except DjangoUser.UserDoesNotExist:
 #            return None
 #
+
+from django.contrib.auth.models import Group
+
 def LigoAuthContext(request):
     #return { 'ligouser' : request.ligouser, 'user' : request.user }
-    return { 'user' : request.user }
+
+    internal_groups = Group.objects.filter(name__in=['Communities:LSCVirgoLIGOGroupMembers', 'executives'])
+
+    user_is_internal = False
+    if request.user:
+        if set(list(internal_groups)) & set(list(request.user.groups.all())):
+            user_is_internal = True
+
+    return { 'user' : request.user, 'user_is_internal' : user_is_internal }
     return { 'ligouser' : request.user, 'user' : request.user }
