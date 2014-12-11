201a2e4ba79cce1c4ca6446015b7948a9aa846ab
Brian Moe <branson@gravity.phys.uwm.edu>
2014-12-11 09:33:18 -0600

    Fixed ivorn pattern so that events won't be rejected by comet.

--- settings/default.py
+++ settings/default.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 # SkyAlert
 
-SKYALERT_IVORN_PATTERN = "ivo://gwnet#%s"
+SKYALERT_IVORN_PATTERN = "ivo://gwnet/gcn_sender#%s"
 SKYALERT_ROLE          = "test"
 SKYALERT_DESCRIPTION   = "Report of a candidate gravitational wave event"
 SKYALERT_SUBMITTERS = ['Patrick Brady', 'Brian Moe']
