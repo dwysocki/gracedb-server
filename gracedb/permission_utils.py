43aa5175954be4a32f65d8bc25fea8270278ea61
Branson Stephens <branson.stephens@ligo.org>
2015-06-05 15:30:41 -0500

    Fixed bug with new defualt permissions.

--- gracedb/permission_utils.py
+++ gracedb/permission_utils.py
@@ -52,7 +52,7 @@ def assign_default_event_perms(event):
         assign_perm(change_codename, g, event)
 
     # If the event is an MDC event, then we expose it to LV-EM also
-    if event.search.name == 'MDC':
+    if event.search and event.search.name == 'MDC':
         lvem = Group.objects.get(name='gw-astronomy:LV-EM')
         assign_perm(view_codename, lvem, event)
         assign_perm(change_codename, lvem, event)
