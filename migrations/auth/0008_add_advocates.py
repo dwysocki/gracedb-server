5e5ef95ddcd1dd53bc2ec222dac97f80dd052c58
Branson Stephens <branson.stephens@ligo.org>
2015-09-15 08:27:15 -0500

    Added lots more names for the followup advocates.

--- migrations/auth/0008_add_advocates.py
+++ migrations/auth/0008_add_advocates.py
@@ -5,6 +5,32 @@ from django.db import models, migrations
 
 advocate_usernames = [
     'branson.stephens@LIGO.ORG',
+    'sean.mcwilliams@LIGO.ORG',
+    'leo.singer@LIGO.ORG',
+    'samaya.nissanke@LIGO.ORG',
+    'massimiliano.razzano@LIGO.ORG',
+    'giuseppe.greco@LIGO.ORG',
+    'barbara.patricelli@LIGO.ORG',
+    'peter.shawhan@LIGO.ORG',
+    'kendall.ackley@LIGO.ORG',
+    'alexander.urban@LIGO.ORG',
+    'daniel.holz@LIGO.ORG',
+    'b.sathyaprakash@LIGO.ORG',
+    'hsin-yu.chen@LIGO.ORG',
+    'sergei.klimenko@LIGO.ORG',
+    'yiming.hu@LIGO.ORG',
+    'adam.zadrozny@LIGO.ORG',
+    'roy.williams@LIGO.ORG',
+    'marica.branchesi@LIGO.ORG',
+    'benjamin.farr@LIGO.ORG',
+    'shaon.ghosh@LIGO.ORG',
+    'eric.chassandemottin@LIGO.ORG',
+    'reed.essick@LIGO.ORG',
+    'eric.howell@LIGO.ORG',
+    'vassiliki.kalogera@LIGO.ORG',
+    'david.coward@LIGO.ORG',
+    'xilong.fan@LIGO.ORG',
+    'akos.szolgyen@LIGO.ORG',
 ]
 
 def add_advocates_group_and_users(apps, schema_editor):
