e276e3a92f72d024e7fdcda6d6d05e6e2213fbec
Branson Stephens <branson.stephens@ligo.org>
2016-01-30 12:24:30 -0600

    Removed code in default settings to raise exception in case of naive datetime object.

--- settings/default.py
+++ settings/default.py
@@ -2,12 +2,6 @@ from settings_secret import *
 
 USE_TZ = True
 
-import warnings
-warnings.filterwarnings(
-        'error', r"DateTimeField .* received a naive datetime",
-        RuntimeWarning, r'django\.db\.models\.fields')
-
-
 # Suitable for production
 ALLOWED_HOSTS = ['*']
 
