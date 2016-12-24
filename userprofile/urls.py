0c1d558c25ea34abc388a157c15db40557fba184
Tanner Prestegard <tanner.prestegard@ligo.org>
2016-12-23 18:37:23 -0600

    adding feature for user testing of contacts, cleaning up twilio alerts a little bit

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,15 +4,27 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('userprofile.views',
-    url (r'^$', 'index', name="userprofile-home"),
-    url (r'^contact/create$', 'createContact', name="userprofile-create-contact"),
-    url (r'^contact/delete/(?P<id>[\d]+)$', 'deleteContact', name="userprofile-delete-contact"),
-    url (r'^contact/edit/(?P<id>[\d]+)$', 'editContact', name="userprofile-edit-contact"),
+    # Base /options/ URL
+    url(r'^$', 'index', name="userprofile-home"),
 
-    url (r'^trigger/create$', 'create', name="userprofile-create"),
-    url (r'^trigger/delete/(?P<id>[\d]+)$', 'delete', name="userprofile-delete"),
-    url (r'^trigger/edit/(?P<id>[\d]+)$', 'edit', name="userprofile-edit"),
+    # /options/contact/
+    url(r'^contact/create$', 'createContact',
+        name="userprofile-create-contact"),
+    url(r'^contact/delete/(?P<id>[\d]+)$', 'deleteContact',
+        name="userprofile-delete-contact"),
+    url(r'^contact/test/(?P<id>[\d]+)$', 'testContact',
+        name="userprofile-test-contact"),
+    url(r'^contact/edit/(?P<id>[\d]+)$', 'editContact',
+        name="userprofile-edit-contact"),
 
-    url (r'^manage_password$', 'managePassword', name="userprofile-manage-password"),
+    # /options/trigger/
+    url(r'^trigger/create$', 'create', name="userprofile-create"),
+    url(r'^trigger/delete/(?P<id>[\d]+)$', 'delete',
+        name="userprofile-delete"),
+    url(r'^trigger/edit/(?P<id>[\d]+)$', 'edit', name="userprofile-edit"),
+
+    # /options/manage_password
+    url(r'^manage_password$', 'managePassword',
+        name="userprofile-manage-password"),
 
 )
