135cf906df27abfdb63a3b28a0ca20c84a7d7a34
Tanner Prestegard <tanner.prestegard@ligo.org>
2017-01-06 15:33:17 -0600

    remove commented out import

--- ligoauth/migrations/0011_add_O2_hwinj_logger_account.py
+++ ligoauth/migrations/0011_add_O2_hwinj_logger_account.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 from django.db import migrations, models
 from django.conf import settings
-#from django.contrib.contenttypes.models import ContentType
 
 ROBOT = {'username': 'hinj',
          'first_name': '',
