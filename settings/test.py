9bd894a63db74d6ae1d7f82bfe5f9b867800c74c
Tanner Prestegard <tanner.prestegard@ligo.org>
2017-04-05 14:51:30 -0500

    removed unused roy.py setting, removed redundant info log, increased size of debug log
    Conflicts:
    	settings/default.py

--- settings/test.py
+++ settings/test.py
@@ -6,18 +6,6 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-DATABASES = {
-    'default' : {
-        'NAME'     : 'gracedb',
-        'ENGINE'   : 'django.db.backends.mysql',
-        'USER'     : 'gracedb',
-        'PASSWORD' : DEFAULT_DB_PASSWORD,
-        'OPTIONS'  : {
-            'init_command' : 'SET storage_engine=MYISAM',
-        },
-    }
-}
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
