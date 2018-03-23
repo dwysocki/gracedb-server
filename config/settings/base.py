ab72e160d21077c760523a0383054a678169f204
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-03-23 11:42:27 -0500

    fixing time zone for this application to be UTC. See https://bugs.ligo.org/redmine/issues/6120

--- config/settings/base.py
+++ config/settings/base.py
@@ -326,7 +326,7 @@ SOUTH_TESTS_MIGRATE = False
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment, this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 GRACE_DATETIME_FORMAT = 'Y-m-d H:i:s T'
 
 # Language code for this installation. All choices can be found here:
