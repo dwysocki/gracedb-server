dd7b53271606aab28e066e8309248e5ca3ebe752
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-05-25 11:32:43 -0500

    changing main view URL for superevents, but keeping legacy-type URLs

--- gracedb/superevents/urls.py
+++ gracedb/superevents/urls.py
@@ -7,8 +7,7 @@ app_name = 'superevents'
 urlpatterns = [
     #url(r'^$', views.index, name="index"),
     #url(r'^create/$', views.create, name="create"),
-    #url(r'^search/(?P<format>(json|flex))?$', views.search, name="search"),
-    url(r'^view/(?P<superevent_id>{regex})/$'.format(
+    url(r'^(?P<superevent_id>{regex})/view/$'.format(
         regex=Superevent.ID_REGEX), views.webview, name="view"),
     url(r'^create_log/(?P<superevent_id>{regex})/$'.format(
         regex=Superevent.ID_REGEX), views.web_create_log, name="create-log"),
@@ -18,4 +17,11 @@ urlpatterns = [
         regex=Superevent.ID_REGEX), views.file_list, name="file-list"),
     url(r'^(?P<superevent_id>{regex})/files/(?P<filename>.*)$'.format(
         regex=Superevent.ID_REGEX), views.file_download, name="file-download"),
+
+    # Legacy URLs for superevent detail view
+    url(r'^(?P<superevent_id>{regex})/$'.format(
+        regex=Superevent.ID_REGEX), views.webview, name="legacyview1"),
+    url(r'^view/(?P<superevent_id>{regex})/$'.format(
+        regex=Superevent.ID_REGEX), views.webview, name="legacyview2"),
+
 ]
