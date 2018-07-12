e95f01c52d5ee79a7884c2530f5e4ec2aac86841
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-07-12 15:14:39 -0500

    Cleanup of superevent web URLs

--- gracedb/superevents/urls.py
+++ gracedb/superevents/urls.py
@@ -4,23 +4,21 @@ from . import views
 
 app_name = 'superevents'
 
+
 # URLs which are nested below a superevent detail
 # These are included under a superevent's ID URL prefix (see below)
 suburlpatterns = [
 
     # Superevent detail view
     url(r'^view/$', views.SupereventDetailView.as_view(), name="view"),
-    #url(r'^(?P<superevent_id>{regex})/oldview/$'.format(
-    #    regex=Superevent.ID_REGEX), views.old_webview, name="oldview"),
-    #url(r'^(?P<superevent_id>{regex})/create_log/$'.format(
-    #    regex=Superevent.ID_REGEX), views.web_create_log, name="create-log"),
 
     # Confirm as GW
     url(r'^confirm_as_gw/$', views.confirm_as_gw, name="confirm-gw"),
 
     # Files
     url(r'^files/$', views.file_list, name="file-list"),
-    url(r'^files/(?P<filename>.*)$', views.file_download, name="file-download"),
+    url(r'^files/(?P<filename>.*)$', views.file_download,
+        name="file-download"),
 
     # Changing LV-EM observers' superevent view/change permissions
     url(r'^perms/$', views.modify_permissions, name="modify-permissions"),
@@ -29,13 +27,16 @@ suburlpatterns = [
     url(r'^signoff/$', views.modify_signoff, name="modify-signoff"),
 ]
 
-# Legacy URL patterns
+# Legacy URL patterns - don't really need them, but we use them for the
+# convenience of users who may be accustomed to the legacy event URL patterns
 legacy_urlpatterns = [
     # Legacy URLs for superevent detail view
     url(r'^(?P<superevent_id>{regex})/$'.format(
-        regex=Superevent.ID_REGEX), views.SupereventDetailView.as_view(), name="legacyview1"),
+        regex=Superevent.ID_REGEX), views.SupereventDetailView.as_view(),
+        name="legacyview1"),
     url(r'^view/(?P<superevent_id>{regex})/$'.format(
-        regex=Superevent.ID_REGEX), views.SupereventDetailView.as_view(), name="legacyview2"),
+        regex=Superevent.ID_REGEX), views.SupereventDetailView.as_view(),
+        name="legacyview2"),
 ]
 
 # Full urlpatterns: legacy urls plus suburlpatterns nested under
