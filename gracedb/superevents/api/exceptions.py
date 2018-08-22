ce9f2be9d25c60b5e58056ecaa7b2715a9e53371
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-08-22 13:42:54 -0500

    Trying to make API exceptions nicer

--- gracedb/superevents/api/exceptions.py
+++ gracedb/superevents/api/exceptions.py
@@ -1,21 +1,25 @@
+import logging
+
 from rest_framework.views import exception_handler
 
-import logging
+# Set up logger
 logger = logging.getLogger(__name__)
+
 
 def gracedb_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
 
-    # Combine values into one list
-    exc_out = [item for sublist in exc.detail.values() for item in sublist]
+    if hasattr(exc, 'detail') and hasattr(exc.detail, 'values'):
+        # Combine values into one list
+        exc_out = [item for sublist in exc.detail.values() for item in sublist]
 
-    # For only one exception, just print it rather than the list
-    if len(exc_out) == 1:
-        exc_out = exc_out[0]
+        # For only one exception, just print it rather than the list
+        if len(exc_out) == 1:
+            exc_out = exc_out[0]
 
-    # Update response data
-    response.data = exc_out
+        # Update response data
+        response.data = exc_out
 
     return response
