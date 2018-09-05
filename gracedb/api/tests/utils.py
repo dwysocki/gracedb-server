c456b948d9c1e3719692b27158c0477900b6ede5
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-09-05 10:27:40 -0500

    Adding optional kwargs to API unit test base

--- gracedb/api/tests/utils.py
+++ gracedb/api/tests/utils.py
@@ -6,7 +6,7 @@ from core.tests.utils import GraceDbTestBase
 class GraceDbApiTestBase(GraceDbTestBase):
     client_class = APIClient
 
-    def request_as_user(self, url, method, user=None, data=None):
+    def request_as_user(self, url, method, user=None, data=None, **kwargs):
         """Shortcut function for making a request to the API"""
         # Get client method for HTTP method requested
         try:
@@ -22,6 +22,8 @@ class GraceDbApiTestBase(GraceDbTestBase):
                 'HTTP_ISMEMBEROF': ';'.join([g.name for g in
                     user.groups.all()]),
             }
+            if kwargs:
+                user_dict.update(**kwargs)
 
             # Make request and return response
             return method_func(url, data, **user_dict)
