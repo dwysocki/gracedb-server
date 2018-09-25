528b37caea341c4b1a4edd6ab38e10e93a96195e
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-09-25 09:56:50 -0500

    Moving test utility from API base to generic base
    Moved request_as_user from GraceDbApiTestBase to GraceDbTestBase
    so that it can be used in tests for other apps.

--- gracedb/api/tests/utils.py
+++ gracedb/api/tests/utils.py
@@ -5,28 +5,3 @@ from core.tests.utils import GraceDbTestBase
 
 class GraceDbApiTestBase(GraceDbTestBase):
     client_class = APIClient
-
-    def request_as_user(self, url, method, user=None, data=None, **kwargs):
-        """Shortcut function for making a request to the API"""
-        # Get client method for HTTP method requested
-        try:
-            method_func = getattr(self.client, method.lower())
-        except Exception as e:
-            raise ValueError('{method} is not a valid HTTP method'.format(
-                method=method))
-
-        if user is not None:
-            # Set up user dict
-            user_dict = {
-                'HTTP_REMOTE_USER': user.username,
-                'HTTP_ISMEMBEROF': ';'.join([g.name for g in
-                    user.groups.all()]),
-            }
-            if kwargs:
-                user_dict.update(**kwargs)
-
-            # Make request and return response
-            return method_func(url, data, **user_dict)
-        else:
-            # Anonymous user
-            return method_func(url, data)
--- gracedb/core/tests/utils.py
+++ gracedb/core/tests/utils.py
@@ -272,5 +272,33 @@ class PublicGroupSetup(TestCase):
 
 class GraceDbTestBase(DefineTestSettings, InternalGroupAndUserSetup,
     LvemGroupAndUserSetup, PublicGroupSetup):
-    """Combines all test base classes"""
-    pass
+    """
+    Combines all test base classes and defines method for easily making
+    requests with a specfic user account.
+    """
+
+    def request_as_user(self, url, method, user=None, data=None, **kwargs):
+        """Shortcut function for making a request"""
+        # Get client method for HTTP method requested
+        try:
+            method_func = getattr(self.client, method.lower())
+        except Exception as e:
+            raise ValueError('{method} is not a valid HTTP method'.format(
+                method=method))
+
+        if user is not None:
+            # Set up user dict
+            user_dict = {
+                'HTTP_REMOTE_USER': user.username,
+                'HTTP_ISMEMBEROF': ';'.join([g.name for g in
+                    user.groups.all()]),
+            }
+            if kwargs:
+                user_dict.update(**kwargs)
+
+            # Make request and return response
+            return method_func(url, data, **user_dict)
+        else:
+            # Anonymous user
+            return method_func(url, data)
+
