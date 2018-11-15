5f2ad8e72cead1badb40c1ed20fedfaebed39264
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-11-15 14:28:04 -0600

    Remove userprofile URLs for which views just return a 404
    URLs for editing contacts and notifications were defined, but the
    view functions just returned 404 responses.  So what's the point
    in having them at all?

--- gracedb/userprofile/urls.py
+++ gracedb/userprofile/urls.py
@@ -14,14 +14,14 @@ urlpatterns = [
         name="userprofile-delete-contact"),
     url(r'^contact/test/(?P<id>[\d]+)$', views.testContact,
         name="userprofile-test-contact"),
-    url(r'^contact/edit/(?P<id>[\d]+)$', views.editContact,
-        name="userprofile-edit-contact"),
+    #url(r'^contact/edit/(?P<id>[\d]+)$', views.editContact,
+    #    name="userprofile-edit-contact"),
 
     # /options/trigger/
     url(r'^trigger/create$', views.create, name="userprofile-create"),
     url(r'^trigger/delete/(?P<id>[\d]+)$', views.delete,
         name="userprofile-delete"),
-    url(r'^trigger/edit/(?P<id>[\d]+)$', views.edit, name="userprofile-edit"),
+    #url(r'^trigger/edit/(?P<id>[\d]+)$', views.edit, name="userprofile-edit"),
 
     # /options/manage_password
     url(r'^manage_password$', views.managePassword,
