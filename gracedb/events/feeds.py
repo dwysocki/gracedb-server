addf608813ffc90023a596be1928c5975eed28bc
Tanner Prestegard <tanner.prestegard@ligo.org>
2018-12-04 13:21:04 -0600

    Bugfix to feeds views
    Restrict access to RSS feeds to internal users

--- gracedb/events/feeds.py
+++ gracedb/events/feeds.py
@@ -1,24 +1,32 @@
-
-from django.contrib.syndication.views import FeedDoesNotExist
-from django.contrib.syndication.views import Feed
-
-from django.urls import reverse
-from django.template import RequestContext
-from django.shortcuts import render
-
-from .models import Event, Group, Pipeline
-#from .views import view, search, index
-from .views import view
-
-from .permission_utils import internal_user_required
+import logging
 
 from django.conf import settings
+from django.contrib.syndication.views import FeedDoesNotExist
+from django.contrib.syndication.views import Feed
+from django.shortcuts import render
+from django.template import RequestContext
+from django.urls import reverse
+
+from .models import Event, Group, Pipeline
+from .permission_utils import internal_user_required
+from .views import view
+
+
+# Set up logger
+logger = logging.getLogger(__name__)
+
 FEED_MAX_RESULTS = getattr(settings, 'FEED_MAX_RESULTS', 20)
+
 
 class EventFeed(Feed):
     title_template = "feeds/latest_title.html"
     description_template = "feeds/latest_description.html"
+
+    # Restrict access to feeds
     @internal_user_required
+    def __call__(self, request, *args, **kwargs):
+        return super(EventFeed, self).__call__(request, *args, **kwargs)
+
     def get_object(self, request, url):
         bits = url.split('/')[1:]
         # bits will look like
