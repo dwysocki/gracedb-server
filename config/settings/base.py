f9a864b6b58ba8d8a1b4ee73ce0b3255495e6480
Tanner Prestegard <tanner.prestegard@ligo.org>
2019-08-21 11:38:38 -0500

    bump version to 2.7.0

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,7 +26,7 @@ MAINTENANCE_MODE = False
 MAINTENANCE_MODE_MESSAGE = None
 
 # Version ---------------------------------------------------------------------
-PROJECT_VERSION = '2.6.3'
+PROJECT_VERSION = '2.7.0'
 
 # Unauthenticated access ------------------------------------------------------
 # This variable should eventually control whether unauthenticated access is
