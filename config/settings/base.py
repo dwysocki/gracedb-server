8720b3bbf649f74051ceb394a71af88bd43d01db
alexander.pace@ligo.org <alexander.pace@ligo.org>
2019-12-31 14:44:35 -0600

    bumping version number

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,7 +26,7 @@ MAINTENANCE_MODE = False
 MAINTENANCE_MODE_MESSAGE = None
 
 # Version ---------------------------------------------------------------------
-PROJECT_VERSION = '2.8.0'
+PROJECT_VERSION = '2.8.1'
 
 # Unauthenticated access ------------------------------------------------------
 # This variable should eventually control whether unauthenticated access is
