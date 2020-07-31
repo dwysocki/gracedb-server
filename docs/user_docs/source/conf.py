588f4eebf5092d305b3183d8ae58da20fac7f69b
alexander.pace@ligo.org <alexander.pace@ligo.org>
2020-07-31 13:51:49 -0500

    title bar fix

--- docs/user_docs/source/conf.py
+++ docs/user_docs/source/conf.py
@@ -114,7 +114,10 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
 html_theme = 'bootstrap'
+html_translator_class = 'bootstrap.HTMLTranslator'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,9 +125,6 @@ html_theme = 'bootstrap'
 html_theme_options = {
     'navbar_title': "GraceDB",
     'navbar_site_name': "Doc", 
-    'googlewebfont': True,
-    'googlewebfont_url': 'http://fonts.googleapis.com/css?family=Text+Me+One',
-    'googlewebfont_style': "font-family: 'Text Me One', sans-serif",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
