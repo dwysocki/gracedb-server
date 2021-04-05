d800b6d8f4c49d0d6af5757320c51a4810c6387c
Alexander Pace <alexander.pace@ligo.org>
2021-04-05 13:09:16 -0500

    updating version tag

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,7 +30,7 @@ INFO_BANNER_ENABLED = False
 INFO_BANNER_MESSAGE = "TEST MESSAGE"
 
 # Version ---------------------------------------------------------------------
-PROJECT_VERSION = '2.11.1'
+PROJECT_VERSION = '2.11.1-1'
 
 # Unauthenticated access ------------------------------------------------------
 # This variable should eventually control whether unauthenticated access is
