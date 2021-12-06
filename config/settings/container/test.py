77f5ffed9a527ec231bbc11a7c8485efe02b1519
Alexander Pace <alexander.pace@ligo.org>
2021-12-06 15:18:59 -0600

    changing info list and (minorly) bumping version number

--- config/settings/container/test.py
+++ config/settings/container/test.py
@@ -90,7 +90,18 @@ INSTANCE_TITLE = 'GraceDB Testing Server'
 INSTANCE_LIST = INSTANCE_STUB.format(ENABLED[SEND_PHONE_ALERTS],
                                 ENABLED[SEND_EMAIL_ALERTS],
                                 LVALERT_OVERSEER_INSTANCES[0]['lvalert_server'],
+                                ENABLED[SEND_XMPP_ALERTS],
+                                LVALERT_OVERSEER_INSTANCES[1]['lvalert_server'],
                                 ENABLED[SEND_XMPP_ALERTS])
+
+# Add sub-bullet with igwn-alert group:
+if (len(LVALERT_OVERSEER_INSTANCES) == 2):
+    igwn_alert_group = os.environ.get('IGWN_ALERT_GROUP', 'lvalert-dev')
+    group_sub_bullet = """<ul>
+    <li> Messages are sent to group: <span class="text-monospace"> {0}  </span></li>
+    </ul>""".format(igwn_alert_group)
+    INSTANCE_LIST = INSTANCE_LIST + group_sub_bullet
+
 INSTANCE_INFO = """
 <h5>Testing Instance</h5>
 <hr>
