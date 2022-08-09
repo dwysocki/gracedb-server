336e3e027da5718a535a164319b9ba0d8fd3032a
Alexander Pace <alexander.pace@ligo.org>
2022-08-09 14:11:07 -0500

    try installing citext to test database

--- config/settings/container/base.py
+++ config/settings/container/base.py
@@ -257,6 +257,9 @@ DATABASES = {
         'HOST': os.environ.get('DJANGO_DB_HOST', ''),
         'PORT': os.environ.get('DJANGO_DB_PORT', ''),
         'CONN_MAX_AGE': 3600,
+        'TEST' : {
+            'NAME': 'gracedb_test_db',
+        },
     },
 }
 
