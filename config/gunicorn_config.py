ea928f99521b4257c0c564b6d7894611483f7307
Alexander Pace <alexander.pace@ligo.org>
2022-09-13 05:47:46 -0500

    redo gunicorn worker settings

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -6,6 +6,14 @@ from os.path import abspath, dirname, join
 import sys
 import multiprocessing
 
+# Useful function for getting environment variables
+def get_from_env(envvar, default_value=None, fail_if_not_found=True):
+    value = os.environ.get(envvar, default_value)
+    if (value == default_value and fail_if_not_found):
+        raise ImproperlyConfigured(
+            'Could not get environment variable {0}'.format(envvar))
+    return value
+
 # Parameters
 GUNICORN_PORT = 8080
 LOG_DIR = abspath(join(dirname(__file__), "..", "..", "logs"))
@@ -14,26 +22,57 @@ LOG_DIR = abspath(join(dirname(__file__), "..", "..", "logs"))
 # Bind to localhost on specified port
 bind = "127.0.0.1:{port}".format(port=GUNICORN_PORT)
 
-# Number of workers = 2*CPU + 1 (recommendation from Gunicorn documentation)
-workers = multiprocessing.cpu_count()*2 + 1
+# Number of workers -----------------------------------------------------------
+# 2*CPU + 1 (recommendation from Gunicorn documentation)
 
-# Worker class.
-# 
-worker_class = 'sync'
+workers  = get_from_env('GUNICORN_WORKERS',
+                   default_value=multiprocessing.cpu_count()*2 + 1,
+                   fail_if_not_found=False)
 
-# Adding options for timeout. Not specified, the timeout default 
-# is 30 seconds. Source:
-#
+threads = get_from_env('GUNICORN_THREADS',
+                   default_value=2,
+                   fail_if_not_found=False)
+
+# Worker class ----------------------------------------------------------------
+# sync by default, generally safe and low-resource:
+# https://docs.gunicorn.org/en/stable/design.html#sync-workers
+
+worker_class = get_from_env('GUNICORN_WORKER_CLASS',
+                   default_value='gthread',
+                   fail_if_not_found=False)
+
+# Timeout ---------------------------------------------------------------------
+# If not specified, the timeout default is 30 seconds:
 # https://gunicorn-docs.readthedocs.io/en/stable/settings.html#worker-processes
-#
-timeout = 300
 
-# Max requests settings - a worker restarts after handling this many
-# requests. May be useful if we have memory leak problems.
+timeout = get_from_env('GUNICORN_TIMEOUT',
+                   default_value=300,
+                   fail_if_not_found=False)
+
+# max_requests settings -------------------------------------------------------
+# The maximum number of requests a worker will process before restarting.
+# May be useful if we have memory leak problems.
 # The jitter is drawn from a uniform distribution:
 # randint(0, max_requests_jitter)
-#max_requests = 0
-#max_requests_jitter = 0
+
+max_requests = get_from_env('GUNICORN_MAX_REQUESTS',
+                   default_value=100,
+                   fail_if_not_found=False)
+
+max_requests_jitter = get_from_env('GUNICORN_MAX_REQUESTS_JITTER',
+                   default_value=10,
+                   fail_if_not_found=False)
+
+# keepalive -------------------------------------------------------------------
+# The number of seconds to wait for requests on a Keep-Alive connection.
+# Generally set in the 1-5 seconds range for servers with direct connection
+# to the client (e.g. when you don’t have separate load balancer).
+# When Gunicorn is deployed behind a load balancer, it often makes sense to set
+# this to a higher value.
+
+keepalive = get_from_env('GUNICORN_KEEPALIVE',
+                   default_value=10,
+                   fail_if_not_found=False)
 
 
 # Logging ---------------------------------------------------------------------
