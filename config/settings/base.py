acd965df6d7fa121a06416147c5a2055e20a1fd8
Daniel Wysocki <daniel.m.wysocki@gmail.com>
2023-02-21 19:15:59 -0500

    Enable Mattermost alerts by default

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,6 +135,7 @@ LOGOUT_REDIRECT_URL = 'home'
 SEND_XMPP_ALERTS = False
 SEND_PHONE_ALERTS = False
 SEND_EMAIL_ALERTS = False
+SEND_MATTERMOST_ALERTS = True
 
 # igwn-alert group settings. the default development group is 'lvalert-dev'
 # for the container deployments, the variable will be overwriten by the 
