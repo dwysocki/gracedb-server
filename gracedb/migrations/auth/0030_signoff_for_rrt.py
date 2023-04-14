94cbf5f82e6296e466360993cfdaa232b2e5e602
Alexander Pace <alexander.pace@ligo.org>
2023-04-14 09:43:47 -0500

    new strategy for rrt perms

--- gracedb/migrations/auth/0030_signoff_for_rrt.py
+++ gracedb/migrations/auth/0030_signoff_for_rrt.py
@@ -6,27 +6,12 @@ from django.conf import settings
 from django.db import migrations
 from django.contrib.auth.management import create_permissions
 
+# New strategy to avoid database integrity errors: simply migrate
+# permissions from the em_advocates group to the rrt group. 
 
 # Group name
 RRT = settings.RRT_MEMBERS_GROUP
-
-ADVOCATE_PERMS = [
-    # Signoff permissions
-    'add_signoff',
-    'change_signoff',
-    'delete_signoff',
-    'do_adv_signoff',
-    ]
-
-
-# We have to run this to force the permissions to actually be created.
-# Otherwise they are created by a post-migrate signal and it's not possible
-# to run all of the migrations in a single command
-def create_perms(apps, schema_editor):
-    for app_config in apps.get_app_configs():
-        app_config.models_module = True
-        create_permissions(app_config, apps=apps, verbosity=0)
-        app_config.models_module = None
+EMA = settings.EM_ADVOCATE_GROUP
 
 
 def add_perms(apps, schema_editor):
@@ -34,26 +19,20 @@ def add_perms(apps, schema_editor):
     Permission = apps.get_model('auth', 'Permission')
 
     rrt_group = Group.objects.get(name=RRT)
+    ema_group = Group.objects.get(name=EMA)
 
-    # Add superevent permissions to groups. Or groups to permissions?
-    for codename in ADVOCATE_PERMS:
-        p = Permission.objects.get(codename=codename,
-            content_type__app_label='superevents')
-        p.group_set.add(rrt_group)
-
+    for perm in ema_group.permissions.all():
+        rrt_group.permissions.add(perm)
 
 def remove_perms(apps, schema_editor):
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
 
     rrt_group = Group.objects.get(name=RRT)
+    ema_group = Group.objects.get(name=EMA)
 
-    # remove permissions from group
-    for codename in ADVOCATE_PERMS:
-        p = Permission.objects.get(codename=codename,
-            content_type__app_label='superevents')
-        p.group_set.remove(rrt_group)
-
+    for perm in ema_group.permissions.all():
+        rrt_group.permissions.remove(perm)
 
 class Migration(migrations.Migration):
 
@@ -63,6 +42,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_perms, migrations.RunPython.noop),
         migrations.RunPython(add_perms, remove_perms),
     ]
