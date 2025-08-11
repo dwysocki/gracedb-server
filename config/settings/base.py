aa92e989d5dbce0a4c0d6b68855ffbc5a6f0313a
Alexander Pace <alexander.pace@ligo.org>
2025-08-11 17:03:31 +0000

    gracedb-2.32.0

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,7 +56,7 @@ INFO_BANNER_MESSAGE = "TEST MESSAGE"
 BETA_REPORTS_LINK = False
 
 # Version ---------------------------------------------------------------------
-PROJECT_VERSION = '2.31.3'
+PROJECT_VERSION = '2.32.0'
 
 # Unauthenticated access ------------------------------------------------------
 # This variable should eventually control whether unauthenticated access is
